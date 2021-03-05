7236cb4255ad82cbbdeeff600166ca79cab3b121
adrianastancu471 <adriana.stancu95@gmail.com>
2021-03-05 23:26:04 +0200

    Initial commit: setup instructions and first approach of octrng driver

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# isabelle_driver
+
+## C-to-Isabelle and AutoCorres applied on Openbsd driver
+
+These are the settings required to install C-to-Isabelle parser and AutoCorres tool.
+
+These will be used from Jedit (an IDE for Isabelle).
+
+## Installation
+
+### Dependencies
+For Ubuntu use the commands bellow:
+
+_For other systems (Debian/MacOS) see: https://github.com/seL4/l4v/blob/master/docs/setup.md_
+
+    sudo apt-get install \
+    python3 python3-pip python3-dev \
+    gcc-arm-none-eabi build-essential libxml2-utils ccache \
+    ncurses-dev librsvg2-bin device-tree-compiler cmake \
+    ninja-build curl zlib1g-dev texlive-fonts-recommended \
+    texlive-latex-extra texlive-metapost texlive-bibtex-extra \
+    mlton-compiler haskell-stack &&
+    stack upgrade --binary-only
+    
+
+The build system for the seL4 kernel requires several python packages:
+
+    sudo pip3 install --upgrade pip
+    sudo pip3 install sel4-deps
+    
+
+### Repo download
+Download the L4v sources using git-repo tool:
+
+_source: https://github.com/seL4/verification-manifest_
+
+    mkdir verification
+    cd verification/
+    
+
+Because of some error (maybe caused by a different combination of versions of python and git-repo): 
+
+    File "/home/user/verification/.repo/repo/main.py", line 79
+        file=sys.stderr)
+        ^
+    SyntaxError: invalid syntax
+    
+
+Download and install git-repo from here:
+
+    curl https://storage.googleapis.com/git-repo-downloads/repo-1 > repo
+    sudo mv repo /usr/bin/
+    python3 /usr/bin/repo init -u https://github.com/seL4/verification-manifest.git
+    python3 /usr/bin/repo sync
+    
+
+### Isabelle installation
+
+_source: https://github.com/seL4/l4v/blob/master/docs/setup.md_
+
+To set up Isabelle for use in l4v/, assuming you have no previous installation of Isabelle, run the following commands:
+
+    cd verification/l4v
+    mkdir -p ~/.isabelle/etc
+    cp -i misc/etc/settings ~/.isabelle/etc/settings
+    ./isabelle/bin/isabelle components -a
+    ./isabelle/bin/isabelle jedit -bf
+    ./isabelle/bin/isabelle build -bv HOL-Word
+    
+
+Building CParser proof:
+_source: https://github.com/seL4/l4v_
+
+    ./isabelle/bin/isabelle build -d . -v -b CParser
+    
+
+Start Isabelle Jedit: 
+
+    ./isabelle/bin/isabelle jedit -d . -l CParser
+    
---
+++ octrng_driver/octrng_simpl.c
@@ -0,0 +1,93 @@
+/*	$OpenBSD: octrng.c,v 1.9 2020/05/29 04:42:24 deraadt Exp $	*/
+/*
+ * Copyright (c) 2013 Paul Irofti <paul@irofti.net>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#define OCTRNG_ENTROPY_REG 0
+#define OCTRNG_CONTROL_ADDR 0x0001180040000000ULL
+#define OCTRNG_RESET  (1UL << 3)
+#define OCTRNG_ENABLE_OUTPUT (1UL << 1)
+#define OCTRNG_ENABLE_ENTROPY  (1UL << 0)
+
+static struct reg {
+  unsigned long control_addr;
+} rng_regs;
+
+unsigned long control_addr2;
+
+static void set_register(unsigned long long reg, unsigned long  value) 
+{
+  switch(reg) {
+    case OCTRNG_CONTROL_ADDR:
+      rng_regs.control_addr = value;
+      control_addr2 = value;
+      break;
+    default:
+      rng_regs.control_addr = 0;   
+      control_addr2 = 0;                         
+      break;
+  }
+}
+
+static unsigned long get_register(unsigned long long reg) 
+{
+  switch(reg) {
+    case OCTRNG_ENTROPY_REG:
+      if ((rng_regs.control_addr&OCTRNG_ENABLE_OUTPUT) &&
+        (rng_regs.control_addr&OCTRNG_ENABLE_ENTROPY))
+         return 13;
+      break;
+    case OCTRNG_CONTROL_ADDR:
+      return rng_regs.control_addr;
+    default:
+      break;
+  }
+  return 0;
+}
+
+void
+octrng_attach(void)
+{
+	unsigned long control_reg;
+
+	control_reg = get_register(OCTRNG_CONTROL_ADDR);
+	control_reg |= (OCTRNG_ENABLE_OUTPUT | OCTRNG_ENABLE_ENTROPY);
+	set_register(OCTRNG_CONTROL_ADDR,control_reg);
+/*
+	timeout_set(&sc->sc_to, octrng_rnd, sc);
+
+	timeout_add_sec(&sc->sc_to, 5);*/
+
+//	printf("Init end \n");
+}
+
+int
+octrng_rnd(void)
+{
+	unsigned int value;
+
+	octrng_attach();
+
+	value = get_register(OCTRNG_ENTROPY_REG);
+
+//	printf("%X ", value);	
+
+return value;
+
+/*
+	enqueue_randomness(value);
+	timeout_add_msec(&sc->sc_to, 10);*/
+}
+
