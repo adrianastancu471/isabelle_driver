b2fa67c3dde0417606f504a62dbe87838ba516a9
adrianastancu471 <adriana.stancu95@gmail.com>
2021-03-24 15:46:07 +0200

    Temporary Run_task wil all the necesarry functions

---
+++ octrng_driver/Run_tasks.thy
@@ -0,0 +1,176 @@
+theory Run_tasks
+imports
+  "CParser.CTranslation"
+  "AutoCorres.AutoCorres"
+begin
+
+declare [[populate_globals=true]]
+
+(*external_file "timeout.c"
+install_C_file "timeout.c"
+
+autocorres [
+  heap_abs_syntax,
+  function_name_suffix="'",
+  lifted_globals_field_suffix="_''" ] "timeout.c"
+
+context timeout begin
+thm timeout_add_msec_body_def
+thm timeout_add_msec'_def
+end*)
+
+external_file "run_tasks.c"
+install_C_file "run_tasks.c" 
+autocorres [
+  heap_abs_syntax,
+  function_name_suffix="'",
+  lifted_globals_field_suffix="_''",
+  ts_force nondet = main 
+ ] "run_tasks.c"
+
+context run_tasks begin
+
+definition "TIMEOUT \<equiv> 100 :: word64"
+definition "MAX_QUEUE \<equiv> 100 :: word32"
+definition "OCTRNG_ENTROPY_REG \<equiv> 0 :: word64"
+definition "OCTRNG_CONTROL_ADDR \<equiv> 0x0001180040000000 :: word64"
+definition "OCTRNG_RESET  \<equiv> (1 << 3) :: word32"
+definition "OCTRNG_ENABLE_OUTPUT \<equiv> (1 << 1) :: word32"
+definition "OCTRNG_ENABLE_ENTROPY  \<equiv> (1 << 0) :: word32"
+
+definition "RNG_ATTACH \<equiv> 1 :: word32"
+definition "RNG_RND \<equiv> 2 :: word32"
+definition "IDLE \<equiv> 3 :: word32"
+
+(* Timeout functions *)
+thm get_time'_def
+thm idle'_def
+thm add_task'_def
+thm get_running_tasks'_def
+thm run_task'_def
+thm call_function'_def
+thm timeout_add_msec_body_def
+thm timeout_add_msec'_def
+
+(* Octrng functions *)
+thm set_register'_def
+thm get_register'_def
+thm octrng_attach'_def
+thm octrng_rnd'_def
+
+(* Main function *)
+thm main'_def
+
+
+
+(* Timeout functions *)
+(* get_time is correct *)
+lemma get_time_correct [simp]: "\<lbrace> \<lambda>s. timer_'' s = a \<rbrace> 
+  get_time'
+  \<lbrace>\<lambda>r s.  r = a \<rbrace>!"
+    unfolding set_register'_def
+    unfolding condition_def
+    unfolding OCTRNG_CONTROL_ADDR_def
+    apply (clarsimp simp: fun_upd_apply)
+    apply wp 
+    apply auto 
+  done 
+
+
+
+
+
+(* Octrng functions *)
+
+(* set_register with global struct *)
+lemma set_reg_control_struct [simp]: "\<lbrace> \<lambda>s. True \<rbrace>
+  set_register' OCTRNG_CONTROL_ADDR a 
+  \<lbrace>\<lambda>_s. control_addr_C (rng_regs_'' s) = a \<rbrace>!"
+    unfolding set_register'_def
+    unfolding condition_def
+    unfolding OCTRNG_CONTROL_ADDR_def
+    apply (clarsimp simp: fun_upd_apply)
+    apply wp 
+    apply auto 
+  done 
+
+(* get_register on OCTRNG_CONTROL_ADDR *)
+lemma get_reg_control [simp]: "\<lbrace>\<lambda>s. control_addr_C (rng_regs_'' s) = a \<rbrace>
+  get_register' OCTRNG_CONTROL_ADDR
+  \<lbrace>\<lambda>r s. r = a \<rbrace>" 
+    unfolding get_register'_def
+    unfolding condition_def
+    unfolding OCTRNG_CONTROL_ADDR_def
+    apply auto 
+    apply wp 
+  done
+
+(* get_register on OCTRNG_ENTROPY_REG *)
+lemma get_reg_entropy [simp]: "\<lbrace>\<lambda>s. 
+  control_addr_C (rng_regs_'' s) && OCTRNG_ENABLE_OUTPUT \<noteq> 0 \<and>
+  control_addr_C (rng_regs_'' s) && OCTRNG_ENABLE_ENTROPY \<noteq> 0 \<and>
+  timer_'' s = a \<rbrace>
+  get_register' OCTRNG_ENTROPY_REG
+  \<lbrace>\<lambda>r s. r = a \<rbrace>"
+    unfolding get_register'_def
+    apply (clarsimp simp:fun_upd_apply)
+    apply wp
+    unfolding OCTRNG_ENTROPY_REG_def OCTRNG_ENABLE_OUTPUT_def OCTRNG_ENABLE_ENTROPY_def
+    unfolding get_time'_def
+    apply auto
+    done
+
+(* octrng_attach sets registers *)
+  thm octrng_attach'_def
+lemma octrng_attach [simp]: "\<lbrace> \<lambda>s. True\<rbrace>
+  octrng_attach' 
+  \<lbrace> \<lambda>_s. control_addr_C (rng_regs_'' s) && (OCTRNG_ENABLE_OUTPUT || OCTRNG_ENABLE_ENTROPY) \<noteq> 0 \<rbrace> "
+    unfolding octrng_attach'_def 
+    unfolding get_register'_def set_register'_def
+    unfolding add_task'_def
+    apply (clarsimp simp:fun_upd_apply)
+    apply wp
+    apply auto
+    unfolding OCTRNG_ENABLE_OUTPUT_def OCTRNG_ENABLE_ENTROPY_def
+    apply auto
+  done
+
+
+(* octrng_rnd gets current time *)
+thm octrng_rnd'_def
+lemma octrng_rnd: "
+  \<lbrace> \<lambda>s. timer_'' s = a \<and> current_task_'' s <  MAX_QUEUE\<rbrace> 
+  octrng_rnd' 
+  \<lbrace> \<lambda>_s. rand_value_'' s = a\<rbrace>!"
+  unfolding octrng_rnd'_def
+  unfolding get_register'_def add_task'_def
+  unfolding get_time'_def
+  unfolding MAX_QUEUE_def
+    apply (clarsimp simp:fun_upd_apply)
+    apply wp
+  apply auto
+ (* additional lemma if registers not set \<rightarrow> timer_'' = 0 *)
+  oops
+
+
+
+(* Main function *)
+lemma main_function: "\<lbrace>\<lambda>s. True\<rbrace> main' \<lbrace>\<lambda>_s. timer_'' s = 100\<rbrace>"
+  unfolding main'_def get_time'_def add_task'_def idle'_def
+    apply (clarsimp simp:fun_upd_apply)
+  apply (subst whileLoop_add_inv
+   [where I="\<lambda>_s.  0 \<le> timer_'' s \<and> timer_'' s \<le> TIMEOUT"
+      and M="\<lambda>s. TIMEOUT - timer_'' s"])
+  unfolding TIMEOUT_def 
+
+ 
+  apply wp
+    apply auto
+ (* apply (subst whileLoop_add_inv
+    [where I="\<lambda>(i') s. 0 \<le> i' \<and> i' \<le> 100"
+        and M="\<lambda>((i'), s). MAX_QUEUE - i'"])*)
+  done
+
+
+
+end
--- octrng_driver/run_tasks.c
+++ octrng_driver/run_tasks.c
@@ -1,24 +1,177 @@
-#include "timeout.h"
-#include "octrng_simpl.h"
+/*#include "timeout.c"
+#include "octrng_simpl.c"*/
 
 #define TIMEOUT 100
+#define MAX_QUEUE 100
 
-int main()
+#define OCTRNG_ENTROPY_REG 0
+#define OCTRNG_CONTROL_ADDR 0x0001180040000000ULL
+#define OCTRNG_RESET  (1UL << 3)
+#define OCTRNG_ENABLE_OUTPUT (1UL << 1)
+#define OCTRNG_ENABLE_ENTROPY  (1UL << 0)
+
+#define RNG_ATTACH 1
+#define RNG_RND 2
+#define IDLE 3
+
+unsigned long rand_value;
+
+static struct reg {
+  unsigned long control_addr;
+} rng_regs;
+
+static unsigned long timer;
+static unsigned int running_tasks;
+static unsigned int current_task;
+
+typedef struct {
+	int timeout;
+	unsigned long long start;
+	int timeout_fun;
+} Task;
+
+Task queue[MAX_QUEUE];
+
+unsigned long 
+get_time(void)
+{
+	return timer;
+}
+
+void 
+idle(void)
+{
+	timer+=1;
+}
+
+int 
+get_running_tasks(void)
+{
+	return running_tasks;
+}
+
+void 
+add_task(int t_fun, int t)
+{
+	if (running_tasks < MAX_QUEUE)
+	{
+		queue[current_task].start = get_time();
+		queue[current_task].timeout = t;
+		queue[current_task].timeout_fun = t_fun;
+		running_tasks++;
+		current_task = (current_task + 1) % MAX_QUEUE;
+	}
+}
+
+void call_function(int fid);
+
+void 
+run_task(int task_id)
+{
+	call_function(queue[task_id].timeout_fun);
+	queue[task_id].timeout_fun = 0;
+	running_tasks--;
+}
+
+void 
+timeout_add_sec(Task* t, unsigned long long sec)
+{
+	t->timeout += sec;
+}
+
+void 
+timeout_add_msec(Task* t, unsigned long long msec)
+{
+	unsigned long long sec = msec * 1000;
+	timeout_add_sec(t, sec);
+}
+
+static void 
+set_register(unsigned long long reg, unsigned long  value) 
+{
+  switch(reg) {
+    case OCTRNG_CONTROL_ADDR:
+      rng_regs.control_addr = value;
+      break;
+    default:
+      rng_regs.control_addr = 0;                   
+      break;
+  }
+}
+
+static unsigned long 
+get_register(unsigned long long reg) 
+{
+  switch(reg) {
+    case OCTRNG_ENTROPY_REG:
+      if ((rng_regs.control_addr&OCTRNG_ENABLE_OUTPUT) &&
+        (rng_regs.control_addr&OCTRNG_ENABLE_ENTROPY))
+         return get_time();
+      break;
+    case OCTRNG_CONTROL_ADDR:
+      return rng_regs.control_addr;
+    default:
+      break;
+  }
+  return 0;
+}
+
+void
+octrng_rnd(void)
+{
+	unsigned int value;
+
+	rand_value = get_register(OCTRNG_ENTROPY_REG);
+    add_task(RNG_RND, 10);
+}
+
+void
+octrng_attach(void)
+{
+	unsigned long control_reg;
+
+	control_reg = get_register(OCTRNG_CONTROL_ADDR);
+	control_reg |= (OCTRNG_ENABLE_OUTPUT | OCTRNG_ENABLE_ENTROPY);
+	set_register(OCTRNG_CONTROL_ADDR,control_reg);
+
+  add_task(RNG_RND, 5);
+}
+
+void 
+call_function(int fid)
+{
+	switch(fid)
+	{
+		case RNG_ATTACH:
+			octrng_attach();
+			break;
+		case RNG_RND:
+			octrng_rnd();
+			break;
+		case IDLE:
+			idle();
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void)
 {
 	int i;
 
-	add_task(octrng_attach, 1);
+	//add_task(RNG_ATTACH, 1);
 
 	while (get_time() < TIMEOUT)
 	{
-		for (i = 0; i < MAX_QUEUE; i++)
+		/*for (i = 0; i < MAX_QUEUE; i++)
 		{
 			if(queue[i].timeout_fun != 0 &&
 				queue[i].timeout <= get_time() - queue[i].start)
 			{
 				run_task(i);
 			}
-		}
+		}*/
 		idle();
 	}
 	return 0;
